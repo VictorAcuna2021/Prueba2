e0026900ff948f0adb8236215345bb4391ce9b4b
VictorAcuna2021 <victor.2001@yahoo.com.ar>
2023-05-29 20:46:17 -0300

    creando el primer archivo

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+let suma = (num1, num2) => num1 + num2;
+let resta = (num1, num2) => num1 - num2;
+let multi = (num1, num2) => num1 * num2;
+let divi = (num1, num2) => num1 / num2;
+
+
+let calculadora = (num1, num2, tipo) => tipo (num1,num2)
+
+console.log(calculadora(10,40,divi));
